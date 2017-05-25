7d43a572c4b0b0835808801d151bcba8fe42de7f
The Thai <thethai.nguyen.212@gmail.com>
2017-05-25 10:48:47 +0700

    styling about page-link to resource

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,17 +1,25 @@
 var React = require('react');
-
-var About = React.createClass({
-  render:function(){
-    return (
-      <h3>About Component</h3>
-    );
-  }
-
-});
+var {Link}=require('react-router')
 
 var About = (props)=>{
   return(
-    <h3>ABout</h3>
+    <div>
+      <h1 className='text-center' >About </h1>
+      <p>This is a weather application build on React.I have built this
+        for The Complete React Web App Developer Course.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <Link to="https://facebook.github.io/react/">React</Link>-this was the js framework used.
+        </li>
+        <li>
+          <a  href="http://openweathermap.org">Open Weather Map</a>I used Open Weatherm Map to search for weather data by city name.
+        </li>
+      </ul>
+    </div>
   )
 };
 
